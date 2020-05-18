720dc31d9d588f03c97700f5c4fd9f9b593c2067
Hugo <33061870+sfcheng222@users.noreply.github.com>
2020-05-19 02:53:55 +0800

    first commit

---
+++ client/main.js
@@ -0,0 +1,117 @@
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter} from 'meteor/ostrio:flow-router-extra';
+
+import './main.html';
+import './register.html';
+import './login.html';
+import './dashboard.html';
+import './home.html';
+
+var Files = new FS.Collection("files", {
+  stores: [new FS.Store.FileSystem("files", {path: "~/uploads"})]
+})
+
+Template.register.events({
+  'click #register_submit'(event, instance) {
+    var username = $('#register_username').val()
+    var email = $('#register_email').val()
+    var password = $('#register_password').val()
+    Accounts.createUser({username: username, email: email, password: password}, function(err) {
+      if(err) console.log(err)
+      if(Meteor.user()) FlowRouter.go('/dashboard')
+    })
+  },
+})
+
+Template.login.events({
+  'click #login_submit'(event, instance) {
+    var email = $('#login_email').val()
+    var password = $('#login_password').val()
+    Meteor.loginWithPassword(email, password, function(err) {
+        if(err) console.log(err)
+        if(Meteor.user()) {
+            FlowRouter.go('/dashboard')
+        } else {
+            console.log("failed to login")
+        }
+    })
+  }
+})
+
+Template.navbar.events({
+  'click #navbar_logout'(event, instance) {
+    Meteor.logout()
+    FlowRouter.go('/login')
+  }
+})
+
+Template.dashboard.onCreated(function() {
+  this.state = new ReactiveDict();
+  this.state.setDefault({ // set default state
+  });
+  var state = this.state
+})
+
+Template.dashboard.helpers({
+  getUploadedFiles: function() {
+    var files =  Files.find({userId: Meteor.userId()})
+    return files
+  },
+  getWindowUrl: function() {
+    return window.location.href
+  },
+  convertSize: function(size) {
+    console.log(size)
+    return (parseInt(size) / 1000).toString().split(".")[0];
+  }
+})
+
+Template.dashboard.events({
+  'click .delete_file'(event, instance) {
+    var fileId = $(event.target).data('id')
+    Files.remove({_id: fileId})
+  },
+  'change #file_upload'(event, instance) {
+    var files = event.currentTarget.files;
+    for(var i=0; i<files.length; i++) {
+      var file = new FS.File(files[i])
+      file.userId = Meteor.user()._id
+      Files.insert(file, function(err, fileObj){
+        console.log("file uploading")
+      })
+    }
+  }
+ })
+
+FlowRouter.route('/', {
+  name: 'home',
+  action: function() {
+    BlazeLayout.render('App_body', {main: 'home'})
+  }
+})
+
+FlowRouter.route('/register', {
+  name: 'register',
+  action: function() {
+    BlazeLayout.render('App_body', {main: 'register'})
+  }
+})
+
+FlowRouter.route('/login', {
+  name: 'login',
+  action: function() {
+    BlazeLayout.render('App_body', {main: 'login'})
+  }
+})
+
+FlowRouter.route('/dashboard', {
+  name: 'dashboard',
+  action: function() {
+    if(Meteor.userId()){
+      BlazeLayout.render('App_body', {main: 'dashboard'})
+    } else {
+      BlazeLayout.render('App_body', {main: 'login'})
+    }
+  }
+})
---
+++ server/main.js
@@ -0,0 +1,34 @@
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+var fs = require('fs');
+var Files = new FS.Collection("files", {
+  stores: [new FS.Store.FileSystem("files", {path: "~/uploads"})],
+})
+
+Files.allow({
+  insert: function() {
+    return true;
+  },
+  download: function(userId, doc) {
+    return doc.userId == userId
+  }
+})
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+Meteor.methods({
+  deleteFile: function(fileName) {
+    console.log("delete file called, "+fileName)
+    var userDirectory = Meteor.user().userDirectory
+    if(userDirectory) {
+      fs.unlinkSync(base+uploadPath+userDirectory+"/"+fileName)
+      filesLink.remove({
+        fileName: fileName,
+        userId: Meteor.user()._id
+      })
+    }
+
+  },
+})
